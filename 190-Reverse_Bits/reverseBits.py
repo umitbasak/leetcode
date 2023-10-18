2287164342c44bbb79ebd09afd4df6e559d45e99
umitbasak <17147986+umitbasak@users.noreply.github.com>
2023-10-18 20:04:26 +0300

    problems 190 and 78 added

---
+++ 190-Reverse_Bits/reverseBits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        output = 0
+
+        for _ in range(0, 32):
+            output <<= 1
+            if n & 1:
+                output += 1
+            n >>= 1
+
+        return output
