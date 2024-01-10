e0d235e1ceac2acb2d89c4cae4b0b567f2949d38
umitbasak <17147986+umitbasak@users.noreply.github.com>
2024-01-10 22:05:37 +0300

    problem 133 added

---
+++ problems/133_clone_graph/clone_graph.py
@@ -0,0 +1,50 @@
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for nei in node.neighbors:
+                copy.neighbors.append(dfs(nei))
+            return copy
+
+        return dfs(node) if node else None
+
+
+node1 = Node()
+node1.val = 1
+node2 = Node()
+node2.val = 2
+node3 = Node()
+node3.val = 3
+node4 = Node()
+node4.val = 4
+
+node1.neighbors.append(node2)
+node1.neighbors.append(node4)
+
+node2.neighbors.append(node1)
+node2.neighbors.append(node3)
+
+node3.neighbors.append(node2)
+node3.neighbors.append(node4)
+
+node4.neighbors.append(node3)
+node4.neighbors.append(node1)
+
+
+solution = Solution()
+solution.cloneGraph(node1)
